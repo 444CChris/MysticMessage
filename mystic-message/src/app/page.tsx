f12f31a205825bad2718e7282c869d79cfa24539
444CChris <christopher.chen444@gmail.com>
2025-07-17 20:41:47 -0600

    Styling changes for home page

--- mystic-message/src/app/page.tsx
+++ mystic-message/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import "./styles/home.css";
 
 export default function Home() {
   return (
@@ -6,9 +7,7 @@ export default function Home() {
       <img src="../Mystic-logo.png"></img>
       <h1>Welcome to mystic message</h1>
       <h2>Connect now for 10 minutes with strangers across the web!</h2>
-      <button>
-        <p>Connect now!</p>
-      </button>
+      <a href="./pages/message.tsx"><p>Connect now!</p></a>
     </div>
   );
 }
